1571d0b9754d1ce7ea0b979606a7985fc3564a02
matt <conradmr@umich.edu>
2025-10-14 20:50:43 -0700

    first commit

---
+++ BreatheWithMe/BreatheWithMeApp.swift
@@ -0,0 +1,18 @@
+//
+//  BreatheWithMeApp.swift
+//  BreatheWithMe
+//
+//  Created on 10/15/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreatheWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
